ffdc9ad5c464c153d301d56f2f85f86005f33195
M. Adnan Masood <m.adnan.masood@gmail.com>
2025-12-29 12:22:39 +0500

    express_api improve get data response and start working on add user

--- code/express-app/index.js
+++ code/express-app/index.js
@@ -4,6 +4,8 @@ const users = require('./users_mock_data.json');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
@@ -17,6 +19,7 @@ menuHtml += "<li><a href='/users'>Users</a></li>";
 menuHtml += "<li><a href='/api/users'>Api Users</a></li>";
 menuHtml += "</ul>";
 
+// Routes for Html Response
 app.get('/', (req, res) => {
   let html = "";
   html += menuHtml;
@@ -38,36 +41,80 @@ app.get('/users', (req, res) => {
   let html = "";
   html += menuHtml;
   html += "<h1>Users</h1>";
-  users.map((user) => {
+  if (!users.data || users.data.length <= 0) {
+    html += "<h3>No Users Found</h3>";
+    return res.send(html);
+  }
+
+  users.data.map((user) => {
     html += getUserHtml(user);
-  });  
+  });
   return res.send(html);
+  
 });
 
 app.get('/users/:id', (req, res) => {
   const id = Number(req.params.id);
-  const user = users.find((user) => {
-    return user.id  === id
-  });
   let html = "";
   html += menuHtml;
-  html += "<h1>User</h1>";
+  html += `<h1>User Id(${id})</h1>`;
+
+  if (!users.data || users.data.length <= 0) {
+    html += "<h3>User not Found</h3>";
+    return res.send(html);
+  }
+
+  const user = users.data.find((user) => {
+    return user.id  === id
+  });
+  if (!user) {
+    html += "<h3>User not Found</h3>";
+    return res.send(html);
+  }
   html += getUserHtml(user);
   return res.send(html);
 });
 
+
+// Route for Json Response
 app.get('/api/users', (req, res) => {
-  return res.json(users);
+  let resp = {status: -1, message: "Unable to fetch users"};
+  if (!users.data || users.data.length <= 0) {
+    return res.json(resp);
+  }
+
+  resp.status = 1;
+  resp.message = 'Users fetched successfully';
+  resp.data = users.data;
+  return res.json(resp);
+});
+
+app.post('/api/users', (req, res) => {
+  const body = req.body;
+  console.log('body', body);
+  return res.json({status: 1, message: 'User created successfully'});
 });
 
 app
   .route('/api/users/:id')
   .get((req, res) => {
+    let resp = {status: -1, message: "Unable to fetch users"};
+    if (!users.data || users.data.length <= 0) {
+      return res.json(resp);
+    }
+
     const id = Number(req.params.id);
-    const user = users.find((user) => {
+    const user = users.data.find((user) => {
       return user.id  === id
     });
-    return res.json(user);
+    if (!user) {
+      resp.message = "User not found";
+      return res.json(resp);
+    }
+    resp.status = 1;
+    resp.message = 'User fetched successfully';
+    resp.data = user;
+    return res.json(resp);
   })
   .patch((req, res) => {
     return res.json({status: 1, message: 'User updated successfully'});
